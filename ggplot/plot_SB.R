edb05725fb64e29ec258907ad32771a444e1e176
Anthony Reinhard <reinhard.aj@gmail.com>
2020-07-18 01:49:04 -0400

    minor tweaks

--- ggplot/plot_SB.R
+++ ggplot/plot_SB.R
@@ -77,10 +77,10 @@ brand_plot <- function(orig_plot, save_name, asp = 1, base_size = 5, data_home =
     ## add axis (or not) to unfaded
     orig_plot <- orig_plot +
       theme(
-        axis.line.y.left = element_line(color = ifelse(grepl('l',fade_borders), 'grey95', 'darkblue')),
+        axis.line.y.left = element_line(color = ifelse(grepl('l',fade_borders), 'transparent', 'darkblue')),
         axis.line.y.right = element_line(color = ifelse(grepl('r',fade_borders), 'grey95', 'darkblue')),
         axis.line.x.top = element_line(color = ifelse(grepl('t',fade_borders), 'grey95', 'darkblue')),
-        axis.line.x.bottom = element_line(color = ifelse(grepl('b',fade_borders), 'grey95', 'darkblue')),
+        axis.line.x.bottom = element_line(color = ifelse(grepl('b',fade_borders), 'transparent', 'darkblue')),
 	panel.border = element_rect(color = 'grey95', size = 0.1),
       )
   }
@@ -106,10 +106,11 @@ theme_SB <-  theme(
   legend.background = element_rect(fill = 'grey90', color = 'darkblue'),
   legend.key = element_blank(),
   panel.grid.minor = element_blank(),
-  panel.grid.major = element_line(color='grey70', size = 0.3),
+  panel.grid.major = element_line(color='grey85', size = 0.3),
   axis.title.y = element_text(angle = 0, vjust = 0.5),
   strip.background = element_blank(),
   strip.text = element_text(size = 6, color = 'darkblue', family = font_SB),
+  legend.position = 'bottom'
 ) 
 
 # StatButler theme for animations
@@ -143,6 +144,9 @@ properLims <- function(vec) {
   plot_min <- ifelse(labs[1] > min(vec, na.rm = T), labs[1] - gap, labs[1])
   return(c(plot_min,plot_max))
 } 
+
+plus_lab = function(x, accuracy = NULL, suffix = '') paste0(ifelse(x>0,'+',''),number(x, accuracy = accuracy, suffix = suffix, scale = ifelse(suffix == '%', 100, 1)))
+plus_lab_format <- function (accuracy = NULL, suffix = '') function(x) plus_lab(x, accuracy = accuracy, suffix = suffix)
 
 # function to set rounded plot limits if scale_x_reverse is used
 properLimsRev <- function(vec) {
@@ -202,17 +206,21 @@ axis_limits_y <- function(p) {
 }
 
 # reads in an image as a grob and allows for some image alterations
-grob_img_adj <- function(img_url, alpha = 0, whitewash = 0) {
+
+grob_img_adj<-function(img_url, alpha = 0, whitewash = 0) {
   return(lapply(img_url, function(x) {
-    img <- image_read(x)[[1]]
-    img[1,,] <- as.raw(255 - (255 - as.integer(img[1,,])) * (1-whitewash))
-    img[2,,] <- as.raw(255 - (255 - as.integer(img[2,,])) * (1-whitewash))
-    img[3,,] <- as.raw(255 - (255 - as.integer(img[3,,])) * (1-whitewash))
-    img[4,,] <- as.raw(as.integer(img[4,,]) * (1-alpha))
-    return(grid::rasterGrob(image = image_read(img)))
+    if(is.na(x)) {
+      return(NULL)
+    }else{     
+      img <- image_read(x)[[1]]
+      img[1,,] <- as.raw(255 - (255 - as.integer(img[1,,])) * (1-whitewash))
+      img[2,,] <- as.raw(255 - (255 - as.integer(img[2,,])) * (1-whitewash))
+      img[3,,] <- as.raw(255 - (255 - as.integer(img[3,,])) * (1-whitewash))
+      img[4,,] <- as.raw(as.integer(img[4,,]) * (1-alpha))
+      return(grid::rasterGrob(image = image_read(img)))
+    }
   }))
 }
-
 
 # used to create branded videos
 Scene2 <- ggproto(
